c90ba4f3ef961785c59efe0bcde74f50b6864331
hamossssdas <40636255+hamossssdas@users.noreply.github.com>
2018-06-27 19:18:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -439,7 +439,7 @@ client.on('message', message => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(``,"http://twitch.tv/S-F")
+client.user.setGame(`KD||3p`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
